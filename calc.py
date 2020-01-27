9543f3cc132c154b7dc299c819e67fdf67de60af
nanda kiran <mnandakiran@gmail.com>
2020-01-27 14:03:26 +0530

    Added calc function

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x ,y):
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+    pass
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x *  y
+
+def divide(x, y):
+    """Divide Function"""
+    return x / y
